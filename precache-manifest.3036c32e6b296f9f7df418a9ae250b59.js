e120575e4b1526b48917e5182ad4dc53368353c3
Usmanqayyum109 <you@example.com>
2022-12-03 23:05:57 +0500

    Updates

---
+++ precache-manifest.3036c32e6b296f9f7df418a9ae250b59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9714a067f097ebb2959",
+    "url": "/Mathematics/static/css/main.8a47d721.chunk.css"
+  },
+  {
+    "revision": "a9714a067f097ebb2959",
+    "url": "/Mathematics/static/js/main.a9714a06.chunk.js"
+  },
+  {
+    "revision": "e45e0075b0567c8b67a9",
+    "url": "/Mathematics/static/js/1.e45e0075.chunk.js"
+  },
+  {
+    "revision": "c9e04fcbb5602c088617",
+    "url": "/Mathematics/static/js/2.c9e04fcb.chunk.js"
+  },
+  {
+    "revision": "56a2587bcb09b4dcb89a",
+    "url": "/Mathematics/static/js/runtime~main.56a2587b.js"
+  },
+  {
+    "revision": "48088297481a73aa024fa672e9e2cb65",
+    "url": "/Mathematics/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Mathematics/precache-manifest.079ef756a209e888de5dfa33ac52ff44.js"
+  "/Mathematics/precache-manifest.3036c32e6b296f9f7df418a9ae250b59.js"
 );
 
 workbox.clientsClaim();
